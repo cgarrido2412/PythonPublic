3f4f6cd9567382e7dc30d16488ad15293dc8fb5c
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-08-15 09:11:09 -0700

    Update sending_email.py

--- sending_email.py
+++ sending_email.py
@@ -1,8 +1,17 @@
 import smtplib
+import datetime
 smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
 smtpObj.ehlo()
 smtpObj.starttls()
 password = str(input('Please enter your password:'))
 smtpObj.login('cgarrido@savers.com', password)
-smtpObj.sendmail('cgarrido@savers.com', 'cgarrido@savers.com', 'Subject: Python.\nI sent this via a script. This is a test to automate cradlepoint e-mails.')
+
+subj = "Cradlepoint"
+date = datetime.datetime.now().strftime( "%d/%m/%Y %H:%M" )
+message_text = "Hello\n This is letting you know you'll be receiving a cradlepoint.\n Have a nice day!"
+from_addr = "Charles Garrido <cgarrido@savers.com>"
+to_addr = "netadmins@savers.com"
+msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % ( from_addr, to_addr, subj, date, message_text )
+
+smtpObj.sendmail('cgarrido@savers.com', 'netadmins@savers.com', msg)
 smtpObj.quit()
