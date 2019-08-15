64af22eeeec8e8e3b39042aaacfaa8dff5ea54c2
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-08-15 08:19:24 -0700

    Update sending_email.py

--- sending_email.py
+++ sending_email.py
@@ -1,5 +1,6 @@
 import smtplib
-smtpObj = smtplib.SMTP('smtp.example.com', 587)
+smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
+
 smtpObj.ehlo()
 smtpObj.starttls()
 smtpObj.login('bob@example.com', ' MY_SECRET_PASSWORD')
