baa75ba492f1ca242542726940700b006b02145d
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-08-15 10:11:47 -0700

    Update sending_email.py
    Changing the variable names to be more generic.

--- sending_email.py
+++ sending_email.py
@@ -5,12 +5,12 @@ smtpObj.ehlo()
 smtpObj.starttls()
 password = str(input('Please enter your password:'))
 smtpObj.login('cgarrido@savers.com', password)
-subj = "TEST - Juniper EX2200 - MGR NODE - Cradlepoint Troubleshooting"
+subj = "Whatever you want the subject of your e-mail to be"
 date = datetime.datetime.now().strftime( "%d/%m/%Y %H:%M" )
-message_text = "Good morning team partners!\n I just wanted to reach out to let you know that during monitoring, we've detected a non-working network switch in the office.\n To cake care of this we're sending a cradlepoint troubleshooting unit that will let us log into the network switch and attempt to reconfigure it.\n \n I will update you shortly with the setup instructions and tracking number, please feel free to reach out if you have any questions!"
+message_text = "Whatever you want the message body to be."
 from_addr = "Charles Garrido <cgarrido@savers.com>"
-to_addr = "charles.garrido@gmail.com"
+to_addr = "someaddress@provider.com"
 msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % ( from_addr, to_addr, subj, date, message_text )
-#Having netadmins@savers.com as second argument BCC's team
-smtpObj.sendmail('cgarrido@savers.com', 'netadmins@savers.com', msg)
+#Having blindcopyaddress@savers.com as second argument BCC's address
+smtpObj.sendmail('cgarrido@savers.com', 'blindcopyaddress@savers.com', msg)
 smtpObj.quit()
