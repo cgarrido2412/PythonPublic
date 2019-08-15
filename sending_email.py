eb298a910cb164222ebdad6d3ca2ddfc2b490a6c
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-08-15 10:15:19 -0700

    generic variables
    made variables more generic

--- sending_email.py
+++ sending_email.py
@@ -4,13 +4,13 @@ smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
 smtpObj.ehlo()
 smtpObj.starttls()
 password = str(input('Please enter your password:'))
-smtpObj.login('cgarrido@savers.com', password)
+smtpObj.login('youremail@outlook.com', password)
 subj = "Whatever you want the subject of your e-mail to be"
 date = datetime.datetime.now().strftime( "%d/%m/%Y %H:%M" )
 message_text = "Whatever you want the message body to be."
-from_addr = "Charles Garrido <cgarrido@savers.com>"
+from_addr = "FirstName LastName <youremail@outlook.com>"
 to_addr = "someaddress@provider.com"
 msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % ( from_addr, to_addr, subj, date, message_text )
 #Having blindcopyaddress@savers.com as second argument BCC's address
-smtpObj.sendmail('cgarrido@savers.com', 'blindcopyaddress@savers.com', msg)
+smtpObj.sendmail('youremail@outlook.com', 'blindcopyaddress@provider.com', msg)
 smtpObj.quit()
