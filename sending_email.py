01bfcec44369de761e7830e5a7f40e7e0a1c17f5
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-08-15 09:22:08 -0700

    Update sending_email.py

--- sending_email.py
+++ sending_email.py
@@ -5,13 +5,11 @@ smtpObj.ehlo()
 smtpObj.starttls()
 password = str(input('Please enter your password:'))
 smtpObj.login('cgarrido@savers.com', password)
-
-subj = "Cradlepoint"
+subj = "TEST - Juniper EX2200 - MGR NODE - Cradlepoint Troubleshooting"
 date = datetime.datetime.now().strftime( "%d/%m/%Y %H:%M" )
-message_text = "Hello\n This is letting you know you'll be receiving a cradlepoint.\n Have a nice day!"
+message_text = "Good morning team partners!\n I just wanted to reach out to let you know that during monitoring, we've detected a non-working network switch in the office.\n To cake care of this we're sending a cradlepoint troubleshooting unit that will let us log into the network switch and attempt to reconfigure it.\n \n I will update you shortly with the setup instructions and tracking number, please feel free to reach out if you have any questions!"
 from_addr = "Charles Garrido <cgarrido@savers.com>"
 to_addr = "netadmins@savers.com"
 msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % ( from_addr, to_addr, subj, date, message_text )
-
 smtpObj.sendmail('cgarrido@savers.com', 'netadmins@savers.com', msg)
 smtpObj.quit()
