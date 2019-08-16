96fbaa1ab4b0f8b7cb718302b1a90cffba902f1f
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-08-16 09:47:09 -0700

    Create monthly outage report production.py

---
+++ monthly outage report production.py
@@ -0,0 +1,31 @@
+#C:\Users\cgarrido\AppData\Local\Programs\Python\Python37\Scripts>
+import pandas as pd
+from pandas import Timestamp
+import pytz
+from pytz import all_timezones
+import datetime
+import xlrd
+import xlwt
+
+data = pd.read_excel('lab.xls')
+data = data.drop_duplicates('Site UP')
+data = data.drop(data[data.Duration == 0].index)
+data['Site DOWN'] = pd.to_datetime(data['Site DOWN'])
+data['Site UP'] = pd.to_datetime(data['Site UP'])
+
+def conversion_function(x: pd.Series) -> pd.Timestamp:  
+    zones = {'Atlantic': 'Canada/Atlantic',
+             'Central': 'US/Central',
+             'Eastern': "US/Eastern",
+             'Mountain': 'US/Mountain',
+             'Pacific': 'US/Pacific',
+	     'Australia': 'Australia/Melbourne',
+	     'Arizona': 'US/Arizona'}
+    raw_time = pd.Timestamp(x[1])
+    loc_raw_time = raw_time.tz_localize("US/Pacific")
+    return loc_raw_time.tz_convert(zones[x[0]]).replace(tzinfo=None)
+
+data['Adjusted_Down'] = data[['Time_Zone', 'Site DOWN']].apply(conversion_function, axis=1)
+data['Adjusted_Up'] = data[['Time_Zone', 'Site UP']].apply(conversion_function, axis=1)
+
+data.to_excel('final.xls', 'a+')
