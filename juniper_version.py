a356e828a57da5ff91d0299420d88c957253c5dd
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-08-19 10:24:09 -0700

    Juniper Documentation
    From Juniper website.

--- juniper_version.py
+++ juniper_version.py
@@ -1,15 +1,19 @@
-#https://www.codementor.io/packt/the-python-api-for-juniper-networks-pr5rdl08c
+#https://www.juniper.net/documentation/en_US/junos/topics/concept/junos-script-automation-python-overview.html
 from ncclient import manager
 
-  conn = manager.connect(
-      host='192.168.24.252',
-      port='830',
-      username='netconf',
-      password='juniper!',
-      timeout=10,
-      device_params={'name':'junos'},
-      hostkey_verify=False)
+def connect(host, port, user, password):
+    conn = manager.connect(host=host,
+            port=port,
+            username=user,
+            password=password,
+            timeout=10,
+            device_params = {'name':'junos'},
+            hostkey_verify=False)
 
-  result = conn.command('show version', format='text')
-  print(result)
-  conn.close_session()
+    print 'show version'
+    print '*' * 30
+    result = conn.command('show version', format='text')
+    print result.xpath('output')[0].text
+
+if __name__ == '__main__':
+    connect('router', '22', 'netconf', 'juniper!')
