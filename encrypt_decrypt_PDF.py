24899673bc5b1b9fb73a0287eeaf64236d3c647d
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-08-21 15:01:37 -0700

    Adding Caesar Encryption / Decryption Functions
    Need to make it work for case sensitive characters.

--- encrypt_decrypt_PDF.py
+++ encrypt_decrypt_PDF.py
@@ -29,3 +29,36 @@ def extract():
     pageObj = pdfReader.getPage(0)
     pageObj.extractText()
     return pageObj.extractText()
+def caesar_encrypt(): #the problem with the encryption method is that it ignores case sensitive characters 
+    message = str(input('Please enter the message to be encrypted: \n'))
+    message = message.upper()
+    text = ''
+    key = int(input('Please enter a number key: \n'))
+    l = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for symbol in message:
+        if symbol in l:
+            n = l.find(symbol)
+            n = n + key
+            if n >= len(l):
+                n = n - len(l)
+            elif n < 0:
+                n = n + len(l)
+            text = text + l[n]
+        else:
+            text = text + symbol
+    print('Your encrypted message is: \n' + text)
+def caesar_decrypt():
+    message = input('Please enter the message to be decrypted: \n')
+    LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for key in range(len(LETTERS)):
+        translated = ''
+        for symbol in message:
+            if symbol in LETTERS:
+                num = LETTERS.find(symbol)
+                num = num - key
+                if num < 0:
+                    num = num + len(LETTERS)
+                translated = translated + LETTERS[num]
+            else:
+                translated = translated + symbol
+        print('Key # %s: %s' % (key, translated))
