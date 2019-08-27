58be11fd6d6f5a904357bfa4b8aaf1ec75ac21e4
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-08-27 08:43:52 -0700

    Update
    Adding comments, moving drop_duplicates function to end of script.

--- pandas_python_excel.py
+++ pandas_python_excel.py
@@ -1,4 +1,8 @@
-#C:\Users\cgarrido\AppData\Local\Programs\Python\Python37\Scripts>
+#Columns should be in the following format before running the script:
+#STORE | REGION | DESIGN | VENDOR | TIME_ZONE | SITE DOWN | SITE UP | DURATION
+
+#open cli and navigate to C:\Users\USERNAME\AppData\Local\Programs\Python\Python37\Scripts>
+#use "pip install APPLICATION" i.e., "pip install pandas" to download modules and allow them to be imported
 import pandas as pd
 from pandas import Timestamp
 import pytz
@@ -8,12 +12,15 @@ import xlrd
 import xlwt
 import time
 
+#Timer for the script starts, opens excel as dataframe and eliminates duplicates + durations with "0" value
 startTime = time.time()
-data = pd.read_excel('lab.xls')
+fileName = str(input('Please enter the filename:'))
+data = pd.read_excel(fileName)
 data = data.drop(data[data.Duration == 0].index)
 data['Site DOWN'] = pd.to_datetime(data['Site DOWN'])
 data['Site UP'] = pd.to_datetime(data['Site UP'])
 
+#Defines time conversion function, looks at value in "Time_Zone" and localizes it to matching dictionary value
 def conversion_function(x: pd.Series) -> pd.Timestamp:  
     zones = {'Atlantic': 'Canada/Atlantic',
              'Central': 'US/Central',
@@ -28,10 +35,12 @@ def conversion_function(x: pd.Series) -> pd.Timestamp:
     loc_raw_time = raw_time.tz_localize("US/Pacific")
     return loc_raw_time.tz_convert(zones[x[0]]).replace(tzinfo=None)
 
+#Application of conversion function, removes duplicate adjusted_up times, then saves
 data['Adjusted_Down'] = data[['Time_Zone', 'Site DOWN']].apply(conversion_function, axis=1)
 data['Adjusted_Up'] = data[['Time_Zone', 'Site UP']].apply(conversion_function, axis=1)
 data = data.drop_duplicates('Adjusted_Up')
-data.to_excel('final.xls', 'a+')
+data.to_excel('python_analyzed_report.xls', 'a+')
 
+#Timer for the script stops, prints total time elapsed within python shell 
 endTime = time.time()
 print('The conversion function took %s seconds to calculate.' % (endTime - startTime))
