dfc0ff8dd14c0b2f51d5e242cfce6518ff3a8424
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-09-05 10:38:57 -0700

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,19 +1,43 @@
 import random
-print('Rock, Paper, Scissors!')
-print('Game Start!')
-print('Choose: Rock, Paper, or Scissors')
-gameChoices = {'rock':1, 'paper':2, 'scissors':3}
-player1choice = str(input('Player one chooses:'))
-player1choice = player1choice.lower()
-computerChoices = ['rock', 'paper', 'scissors']
-computerDecision = random.choice(computerChoices)
-print('Computer chooses:', computerDecision)
-player1compare = gameChoices.get(player1choice)
-computerCompare = gameChoices.get(computerDecision)
-result = player1compare - computerCompare
-if result == 0:
-    print('The game is a draw!')
-elif result in [-2, 1]:
-    print('Player 1 wins!')
-elif result in [-1, 2]:
-    print('Computer wins!')
+while True:
+    print('Rock, Paper, Scissors!')
+    print('Press ctrl+c to quit.')
+    print('Are you playing alone? [y/n]')
+    gameModeEntry = str(input())
+    if gameModeEntry == 'y':
+        print('Game Start!')
+        print('Choose: Rock, Paper, or Scissors')
+        gameChoices = {'rock':1, 'paper':2, 'scissors':3}
+        player1choice = str(input('Player one chooses:'))
+        player1choice = player1choice.lower()
+        computerChoices = ['rock', 'paper', 'scissors']
+        computerDecision = random.choice(computerChoices)
+        print('Computer chooses:', computerDecision)
+        player1compare = gameChoices.get(player1choice)
+        computerCompare = gameChoices.get(computerDecision)
+        result = player1compare - computerCompare
+        if result == 0:
+            print('The game is a draw!')
+        elif result in [-2, 1]:
+            print('Player 1 wins!')
+        elif result in [-1, 2]:
+            print('Computer wins!')
+    elif gameModeEntry == 'n':
+        print('Game Start!')
+        print('Choose: Rock, Paper, or Scissors')
+        gameChoices = {'rock' : 1, 'paper' : 2, 'scissors' : 3}
+        player1choice = str(input('Player one chooses:'))
+        player1choice = player1choice.lower()
+        player2choice = str(input('Player two chooses:'))
+        player2choice = player2choice.lower()
+        player1compare = gameChoices.get(player1choice)
+        player2compare = gameChoices.get(player2choice)
+        result = player1compare - player2compare
+        if result == 0:
+            print('The game is a draw!')
+        elif result in [-2, 1]:
+            print('Player one wins!')
+        elif result in [-1, 2]:
+            print('Player two wins!')
+    else:
+        print('Invalid choice! Please choose either "y" or "n"')
