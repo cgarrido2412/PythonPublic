f33bbc2c907b1d57fb8dbcdc44869bf444306107
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-09-05 10:27:38 -0700

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,19 +1,19 @@
 import random
-import time
-print('rock, paper, scissors!')
-player1 = str(input('Player 1 enter a choice:'))
-gameDict = {'rock' : 1, 'paper' : 2, 'scissors' : 3}
+print('Rock, Paper, Scissors!')
+print('Game Start!')
+print('Choose: Rock, Paper, or Scissors')
+gameChoices = {'rock':1, 'paper':2, 'scissors':3}
+player1choice = str(input('Player one chooses:'))
+player1choice = player1choice.lower()
 computerChoices = ['rock', 'paper', 'scissors']
-computer = random.choice(computerChoices)
-print('computer is thinking...')
-time.sleep(5)
-print('computer chooses:', computer) 
-p1 = gameDict.get(player1)
-c1 = gameDict.get(computer)
-result = p1 - c1 
-if result in [0]:
-    print('Draw')
+computerDecision = random.choice(computerChoices)
+print('Computer chooses:', computerDecision)
+player1compare = gameChoices.get(player1choice)
+computerCompare = gameChoices.get(computerDecision)
+result = player1compare - computerCompare
+if result == 0:
+    print('The game is a draw!')
 elif result in [-2, 1]:
-    print('You win!')
+    print('Player 1 wins!')
 elif result in [-1, 2]:
-    print('Computer wins')
+    print('Computer wins!')
