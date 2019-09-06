dd12d239aa049888c608b57d8ebc763ecc5017c0
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-09-06 10:04:59 -0700

    Update lab.py

--- lab.py
+++ lab.py
@@ -9,5 +9,7 @@ import time
 import numpy as np
 
 data = pd.read_excel('lab.xlsx')
-data['duration'] = data['Adjusted_Up'] - data['Adjusted_Down']
-data['duration'] = data['duration']/np.timedelta64(1, 'm')
+site_open = data[(data.Adjusted_Down >= '09:00:00')]
+site_close = data[(data.Adjusted_Down <= '21:00:00')]
+data['daytime'] = data['site_open'] - data['site_close']
+data['daytime'] = data['daytime']/np.timedelta64(1, 'm')
