1d906aba5bca0784a47fb0d7ca9c42fbe91497da
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-09-10 12:17:20 -0700

    Update lab.py

--- lab.py
+++ lab.py
@@ -12,18 +12,26 @@ import numpy as np
 import xlsxwriter
 
 data = pd.read_excel('lab.xlsx')
+
+#creates new column 'duration' by indexing difference between up and down time. returns float
 data['duration'] = data['Adjusted_Up'] - data['Adjusted_Down']
 data['duration'] = data['duration']/np.timedelta64(1,'m')
 
+#returns a minute by minute breakdown in between Adjusted_Down and Adjusted_Up
 s = data.apply(lambda row: pd.date_range(row['Adjusted_Down'], row['Adjusted_Up'], freq='T'), axis=1).explode()
+
+#returns total amount of downtime between 9-21 but not by store 
 s.dt.time.between(time(9), time(21)).sum()  
 
+#range of index[0] for s 
 slist = range(0, 227)
 
+#due to thy this loop itterates, it returns the same thing as 'duration' column 
 for num in slist:
     x = s[num].count()
     print(x)  
 
+#secondary function to test
 def by_month():
     s = data.apply(lambda row: pd.date_range(row['Adjusted_Down'], row['Adjusted_Up'], freq='T'), axis=1).explode()
     downtime = pd.DataFrame({
