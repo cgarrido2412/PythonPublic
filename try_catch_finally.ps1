59e0cd8072ec55dd10c074b92b3daf5cf190e3e1
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-09-12 09:12:34 -0700

    Create try_catch_finally.ps1

---
+++ try_catch_finally.ps1
@@ -0,0 +1,14 @@
+Try{
+    # Try a series of statement codes here... and/or ForEach etc. 
+
+    # Last line of the block code
+    $status = "Successfully exectued the try block code without any error."
+}
+Catch{
+    # If try block ran into exception, it will execute it here. 
+    $status = "$($_.Exception.Message)"
+    Write-Host "Exception Message: $status"
+}
+Finally{
+    Write-Host $status
+}
