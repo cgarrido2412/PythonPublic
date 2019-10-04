ec0a7dd59597607400df5057863696c1bc526c82
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-10-04 14:54:07 -0700

    Update pygame_lab.py

--- pygame_lab.py
+++ pygame_lab.py
@@ -15,6 +15,9 @@ black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 
+#define block color
+block_color = (53,115,255)
+
 #articulation of car boundaries
 car_width = 73
 
@@ -25,6 +28,12 @@ clock = pygame.time.Clock()
 
 #load the car image
 carImg = pygame.image.load('racecar.png')
+
+#keeping score 
+def things_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: "+str(count), True, black)
+    gameDisplay.blit(text,(0,0))
 
 #define objects for collisions
 def things(thingx, thingy, thingw, thingh, color):
@@ -63,37 +72,58 @@ def game_loop():
     thing_width = 100
     thing_height = 100
     x_change = 0
+    thingCount = 1
+    dodged = 0
     gameExit = False
+
     while not gameExit:
+
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
             if event.type == pygame.KEYDOWN:
+
                 if event.key == pygame.K_LEFT:
                     x_change = -5
+
                 if event.key == pygame.K_RIGHT:
                     x_change = 5
+
             if event.type == pygame.KEYUP:
+
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
+
         x += x_change
         gameDisplay.fill(white)
         things(thing_startx, thing_starty, thing_width, thing_height, black)
         thing_starty += thing_speed
         car(x,y)
+        things_dodged(dodged)
+
         if x > display_width - car_width or x < 0:
             crash()
+
         if thing_starty > display_height:
             thing_starty = 0 - thing_height
             thing_startx = random.randrange(0,display_width)
+            dodged += 1
+            thing_speed += 1
+            thing_width += (dodged * 1.2)
+
         if y < thing_starty+thing_height:
             print('y corssover')
+
             if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx+thing_width:
                 print('x corssover')
                 crash()
+
         pygame.display.update()
         clock.tick(60)
+        
 game_loop()
 pygame.quit()
 quit()
