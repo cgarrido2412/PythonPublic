c2942651244a11d89190f0d9e80859240c88f4ef
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-10-25 10:26:17 -0700

    Update pandas_python_daily.py

--- pandas_python_daily.py
+++ pandas_python_daily.py
@@ -14,84 +14,95 @@ import xlrd
 import xlwt
 import time
 
-start_script()
-day = str(input("Enter the day(integer): "))
-month = str(input('Enter the month(integer): '))
-year = str(input('Enter the year(integer): '))
-filename = (month + '_' + day + '_' + year + '.xls')
-print('filename is: outage_' + filename)
-date = (month + '/' + day + '/' + year)
-storeOpen = '09:00:00'
-storeClose = '21:00:00'
-startTime = time.time()
-data = pd.read_excel('E:\Savers\Spreadsheets\Outage\outage_' + filename, header=[2])
-data = data.drop_duplicates('Site UP') 
-data = data.drop(data[data.Duration == 0].index) 
-data = data.drop(data[data.Duration == 1].index)
-data['Site DOWN'] = pd.to_datetime(data['Site DOWN']) 
-data['Site UP'] = pd.to_datetime(data['Site UP'])
-
-def conversion_function(x: pd.Series) -> pd.Timestamp:
+try:
     
-    zones = {'Atlantic': 'Canada/Atlantic',
-             'Central': 'US/Central',
-             'Eastern': "US/Eastern",
-             'Mountain': 'US/Mountain',
-             'Pacific': 'US/Pacific',
-	     'Australia': 'Australia/Melbourne',
-	     'Arizona': 'US/Arizona',
-             'Alaska': 'US/Alaska',
-             'Hawaii' : 'US/Hawaii'}
+    start_script()
+    day = str(input("Enter the day(integer): "))
+    month = str(input('Enter the month(integer): '))
+    year = str(input('Enter the year(integer): '))
+    filename = (month + '_' + day + '_' + year + '.xls')
+    print('filename is: outage_' + filename)
+    date = (month + '/' + day + '/' + year)
+    storeOpen = '09:00:00'
+    storeClose = '21:00:00'
+    startTime = time.time()
+    try:
+        data = pd.read_excel('E:\Savers\Spreadsheets\Outage\outage_' + filename, header=[2])
+        data = data.drop_duplicates('Site UP') 
+        data = data.drop(data[data.Duration == 0].index) 
+        data = data.drop(data[data.Duration == 1].index)
+        data['Site DOWN'] = pd.to_datetime(data['Site DOWN']) 
+        data['Site UP'] = pd.to_datetime(data['Site UP'])
+
+        def conversion_function(x: pd.Series) -> pd.Timestamp:
+            
+            zones = {'Atlantic': 'Canada/Atlantic',
+                     'Central': 'US/Central',
+                     'Eastern': "US/Eastern",
+                     'Mountain': 'US/Mountain',
+                     'Pacific': 'US/Pacific',
+                     'Australia': 'Australia/Melbourne',
+                     'Arizona': 'US/Arizona',
+                     'Alaska': 'US/Alaska',
+                     'Hawaii' : 'US/Hawaii'}
+            
+            raw_time = pd.Timestamp(x[1])
+            loc_raw_time = raw_time.tz_localize("US/Pacific")
+            return loc_raw_time.tz_convert(zones[x[0]]).replace(tzinfo=None)
+
+        data['Adjusted_Down'] = data[['Time_Zone', 'Site DOWN']].apply(conversion_function, axis=1)
+        data['Adjusted_Up'] = data[['Time_Zone', 'Site UP']].apply(conversion_function, axis=1)
+        data.insert(9, 'Notes', value='')
+        data.to_excel('E:\Savers\Spreadsheets\Outage\outage_' + filename)
+
+        def breakdown(x, y):
+            #First breakdown downtime timestamp. Example string "2019-08-11 10:31:00"
+            string1 = x.split() 
+            variable1 = string1[0] 
+            dateVariable = variable1.split('-') 
+            variable2 = string1[1] 
+            dateVariable2 = variable2.split(':') 
+            hour = int(dateVariable2[0]) 
+            minute = int(dateVariable2[1]) 
+            seconds = int(dateVariable2[2]) 
+
+            #For uptime
+            string1B = y.split()
+            variable1B = string1B[0]
+            dateVariableB = variable1B.split('-')
+            variable2B = string1B[1]
+            dateVariable2B = variable2B.split(':')
+            hourB = int(dateVariable2B[0])
+            minuteB = int(dateVariable2B[1])
+            secondsB = int(dateVariable2B[2])
+
+            if hour in range(9, 21):
+
+                if hourB > hour:
+                    sumMinutes = (hourB - hour)*60
+                    sumMinutes = sumMinutes + (minuteB - minute)
+                    print(sumMinutes)
+
+                elif hourB == hour:
+                    sumMinutes = (minuteB - minute)
+                    print(sumMinutes)
+
+            else:
+                pass
+
+        for index, row in data.iterrows():
+            print(row['Store'], 'Adjusted_Down:', row['Adjusted_Down'], 'Adjusted_Up:', row['Adjusted_Up'])
+            data1 = str(row['Adjusted_Down'])
+            data2 = str(row['Adjusted_Up'])
+            breakdown(data1, data2) 
+
+        endTime = time.time()
+        print('The conversion function took %s seconds to calculate.' % (endTime - startTime))
+
+    except:
+
+        print('File does not exist!')
+
+except:
     
-    raw_time = pd.Timestamp(x[1])
-    loc_raw_time = raw_time.tz_localize("US/Pacific")
-    return loc_raw_time.tz_convert(zones[x[0]]).replace(tzinfo=None)
-
-data['Adjusted_Down'] = data[['Time_Zone', 'Site DOWN']].apply(conversion_function, axis=1)
-data['Adjusted_Up'] = data[['Time_Zone', 'Site UP']].apply(conversion_function, axis=1)
-data.insert(9, 'Notes', value='')
-data.to_excel('E:\Savers\Spreadsheets\Outage\outage_' + filename)
-
-def breakdown(x, y):
-    #First breakdown downtime timestamp. Example string "2019-08-11 10:31:00"
-    string1 = x.split() 
-    variable1 = string1[0] 
-    dateVariable = variable1.split('-') 
-    variable2 = string1[1] 
-    dateVariable2 = variable2.split(':') 
-    hour = int(dateVariable2[0]) 
-    minute = int(dateVariable2[1]) 
-    seconds = int(dateVariable2[2]) 
-
-    #For uptime
-    string1B = y.split()
-    variable1B = string1B[0]
-    dateVariableB = variable1B.split('-')
-    variable2B = string1B[1]
-    dateVariable2B = variable2B.split(':')
-    hourB = int(dateVariable2B[0])
-    minuteB = int(dateVariable2B[1])
-    secondsB = int(dateVariable2B[2])
-
-    if hour in range(9, 21):
-
-        if hourB > hour:
-            sumMinutes = (hourB - hour)*60
-            sumMinutes = sumMinutes + (minuteB - minute)
-            print(sumMinutes)
-
-        elif hourB == hour:
-            sumMinutes = (minuteB - minute)
-            print(sumMinutes)
-
-    else:
-        pass
-
-for index, row in data.iterrows():
-    print(row['Store'], 'Adjusted_Down:', row['Adjusted_Down'], 'Adjusted_Up:', row['Adjusted_Up'])
-    data1 = str(row['Adjusted_Down'])
-    data2 = str(row['Adjusted_Up'])
-    breakdown(data1, data2) 
-
-endTime = time.time()
-print('The conversion function took %s seconds to calculate.' % (endTime - startTime))
+    print('Error, make sure Time_Zone column does not have any missing values.')
