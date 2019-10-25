5e5c9c13507f6ebd3ed0f3db433db0447ffaa971
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-10-25 11:37:16 -0700

    Update juniper_rescue.py

--- juniper_rescue.py
+++ juniper_rescue.py
@@ -9,13 +9,32 @@ dev = Device(host='vMX-1', user='lab', passwd='lab123')
 
 try:
     dev.open()
-    with Config(dev, mode='exclusive') as cu:
-        resue = cu.rescue(action="reload")
-        if rescue is False:
-            print ("No existing rescue configuration.")
-        else:
-            cu.pdiff()
+    
+    #Try to load local configuration file
+    try:
+        conf_file = 'configs/junos-config-interfaces.conf'
+
+        with Config(dev, mode='exclusive') as cu:
+            cu.load(path=conf_file, merge=True)
             cu.commit()
+
+    except:
+        print('Unable to load local configuration file.')
+        #Try to load rescue configuration
+        try:
+            
+            with Config(dev, mode='exclusive') as cu:
+                resue = cu.rescue(action="reload")
+                
+                if rescue is False:
+                    print ("No existing rescue configuration.")
+                    
+                else:
+                    cu.pdiff()
+                    cu.commit()
+                    
+        except:
+            print('Unable to utilize rescue configuration.')
     
 except ConnectError as err:
     print('Cannot connect to device: {0}'.format(err))
