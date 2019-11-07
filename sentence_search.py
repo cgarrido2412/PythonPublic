a845058ae9532a2c8ee9a6521fcb2563bd7a1442
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-11-07 09:29:21 -0800

    Update sentence_search.py

--- sentence_search.py
+++ sentence_search.py
@@ -1,34 +1,56 @@
-file = input('Enter file path:')
-
-try:
-    open_file = open(file)
+def manual():
+    file = input('Enter file path:')
     
-    while True:    
+    try:
         open_file = open(file)
-        counted = dict()
+        
+        while True:    
+            open_file = open(file)
+            counted = dict()
+            
+            for line in open_file:
+                the_sentences = line.split('.')
+                the_words = line.split()
+                
+                for word in the_words:
+                    
+                    if word not in counted:
+                        counted[word] = 1
+                    
+                    else:
+                        counted[word] += 1
+            
+            print(counted)
+            ask = input('Enter a word to search or type "done" to quit: \n')
+            
+            if ask == 'done':
+                break
+            
+            else:
+                print('Searching...')
+                print(counted.get(ask))
+    
+    except:
+        print('File cannot be opened:', open_file)
+        exit()
+        
+def module():
+    import re
+    file = input('Enter file path: \n')
+    
+    try:
+        open_file = open(file)
+        ask = input('Enter a word to search: \n')
+        query = ('(' + ask + ')')
+        query = query.strip()
         
         for line in open_file:
             the_sentences = line.split('.')
-            the_words = line.split()
-            
-            for word in the_words:
-                
-                if word not in counted:
-                    counted[word] = 1
-                
-                else:
-                    counted[word] += 1
-                    
-        print(counted)
-        ask = input('Enter a word to search or type "done" to quit: \n')
         
-        if ask == 'done':
-            break
-            
-        else:
-            print('Searching...')
-            print(counted.get(ask))
-            
-except:
-    print('File cannot be opened:', open_file)
-    exit()
+        for item in the_sentences:
+            result = re.split(query,item)
+            print(result)
+    
+    except:
+        print('File cannot be opened:', open_file)
+        exit()
