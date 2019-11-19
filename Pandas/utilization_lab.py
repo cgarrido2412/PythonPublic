54aae690f425ee5db3cc970b9d168da4c49f2d5d
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-11-19 12:17:51 -0800

    Update utilization_lab.py

--- Pandas/utilization_lab.py
+++ Pandas/utilization_lab.py
@@ -1,20 +1,28 @@
 import pandas as pd
+
 file1 = input('Enter file path: \n')
+
 try:
     open(file1)
+    
 except:
     print('Unable to open:', file1)
     exit()
+    
 file2 = input('Enter second file path: \n')
+
 try:
     open(file2)
+    
 except:
     print('Unable to open:', file2)
     exit()
+    
 def utilization_report(file1, file2):
     data1 = pd.read_excel(file1, header=[2], usecols=range(1,4))
     data2 = pd.read_excel(file2, header=[2], usecols=range(1,4))
     data3 = pd.merge(data2, data1, on='Full Name')
     print(data3)
     data3.to_excel('lab3.xls', 'a+')
+    
 utilization_report(file1, file2)
