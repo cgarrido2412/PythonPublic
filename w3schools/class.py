457b2749efacc9c0df9799c7091bfdd8458bb5fc
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-11-26 09:25:06 -0800

    Create class.py

---
+++ w3schools/class.py
@@ -0,0 +1,11 @@
+#! Python 3
+
+class Person:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+p1 = Person("Charles", 25)
+print(p1.name)
+print(p1.age)
