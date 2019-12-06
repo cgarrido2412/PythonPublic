1a2881009e0346b3be1479ac1c3c4b6a06d88df9
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-12-06 11:30:36 -0800

    Update day6.py

--- adventofcode2019/day6.py
+++ adventofcode2019/day6.py
@@ -1,4 +1,68 @@
 #!Python3
+'''
+--- Day 6: Universal Orbit Map ---
+You've landed at the Universal Orbit Map facility on Mercury. Because navigation in space often involves
+transferring between orbits, the orbit maps here are useful for finding efficient routes between, for example, you
+and Santa. You download a map of the local orbits (your puzzle input).
+
+Except for the universal Center of Mass (COM), every object in space is in orbit around exactly one other object.
+An orbit looks roughly like this:
+
+                  \
+                   \
+                    |
+                    |
+AAA--> o            o <--BBB
+                    |
+                    |
+                   /
+                  /
+In this diagram, the object BBB is in orbit around AAA. The path that BBB takes around AAA (drawn with lines) is
+only partly shown. In the map data, this orbital relationship is written AAA)BBB, which means "BBB is in orbit
+around AAA".
+
+Before you use your map data to plot a course, you need to make sure it wasn't corrupted during the download. To
+verify maps, the Universal Orbit Map facility uses orbit count checksums - the total number of direct orbits (like
+the one shown above) and indirect orbits.
+
+Whenever A orbits B and B orbits C, then A indirectly orbits C. This chain can be any number of objects long: if A
+orbits B, B orbits C, and C orbits D, then A indirectly orbits D.
+
+For example, suppose you have the following map:
+
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+Visually, the above map of orbits looks like this:
+
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I
+In this visual representation, when two objects are connected by a line, the one on the right directly orbits the
+one on the left.
+
+Here, we can count the total number of orbits as follows:
+
+D directly orbits C and indirectly orbits B and COM, a total of 3 orbits.
+L directly orbits K and indirectly orbits J, E, D, C, B, and COM, a total of 7 orbits.
+COM orbits nothing.
+The total number of direct and indirect orbits in this example is 42.
+
+What is the total number of direct and indirect orbits in your map data?
+'''
+
+object_dictionary = {}
+
 file = input('Enter file path and file name: \n')
 
 def file_test(file):
@@ -11,39 +75,39 @@ def file_test(file):
 
 file_test(file)
 
-obj = {}
 with open(file) as working_file:
     
-    for ix, line in enumerate(working_file):
-        data = [str(x.strip()) for x in line.split(')')]
-        a = data[0]
-        b = data[1]
+    for variable, line in enumerate(working_file):
+        data = [str(x.strip()) for x in line.split(')')] #AAA)BBB
+        a = data[0] #AAA
+        b = data[1] #BBB
         
-        if a in obj:
-            obj[a].append(b)
+        if a in object_dictionary:
+            object_dictionary[a].append(b)
             
         else:
-            obj[a] = [b]
+            object_dictionary[a] = [b]
 
-    num = 0
-    q = ['COM']
+    number = 0
     count = 0
+    center_of_mass = ['COM']
     
-    while q:
-        l = len(q)
+    while center_of_mass:
+        length = len(center_of_mass)
         
-        for i in range(l):
-            count += num
+        for kanye in range(length):
+            count += number
             
-            if q[i] in obj:
+            if center_of_mass[kanye] in object_dictionary:
                 
-                for j in obj[q[i]]:
-                    q.append(j)
+                for west in object_dictionary[center_of_mass[kanye]]:
+                    center_of_mass.append(west)
 
-        for i in range(l):
-            q.pop(0)
+        for kanye in range(length):
+            #The pop() method removes the item at the given index from the list and returns the removed item.
+            center_of_mass.pop(0) #list.pop(index) 
 
-        num += 1
+        number += 1
         
     print(count)
     
