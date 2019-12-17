dc1d108d5aa681cb9091473c2d2c9a4c3c9e3caa
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-12-17 15:28:43 -0800

    Update case_sensitive_caesar.py

--- Cipher/case_sensitive_caesar.py
+++ Cipher/case_sensitive_caesar.py
@@ -1,49 +1,104 @@
+#!Python3
+
 def caesar_encrypt():  
     message = str(input('Please enter the message to be encrypted: \n'))
     text = ''
     key = int(input('Please enter a number key: \n'))
     L = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     l = 'abcdefghijklmnopqrstuvwxyz'
+
     for symbol in message:
+
         if symbol in L:
             n = L.find(symbol)
             n = n + key
+
             if n >= len(L):
                 n = n - len(L)
+
             elif n < 0:
                 n = n + len(L)
             text = text + L[n]
+
         elif symbol in l:
             n = l.find(symbol)
             n = n + key
+
             if n >= len(l):
                 n = n - len(l)
+
             elif n < 0:
                 n = n + len(l)
             text = text + l[n]
+
         else:
             text = text + symbol
     print('Your encrypted message is: \n' + text)
+
 def caesar_decrypt():
     message = input('Please enter the message to be decrypted: \n')
     LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     letters = 'abcdefghijklmnopqrstuvwxyz'
+
     for key in range(len(LETTERS)):
         translated = ''
+
         for symbol in message:
+
             if symbol in LETTERS:
                 num = LETTERS.find(symbol)
                 num = num - key
+
                 if num < 0:
                     num = num + len(LETTERS)
                 translated = translated + LETTERS[num]
+
             elif symbol in letters:
                 num = letters.find(symbol)
                 num = num - key
+
                 if num < 0:
                     num = num + len(letters)
                 translated = translated + letters[num]
+
             else:
                 translated = translated + symbol
         print('Key # %s: %s' % (key, translated))
 
+try:
+    mode = str(input('What would you like to do?\n[encrypt][decrypt]\n'))
+    mode = mode.strip()
+    mode = mode.lower()
+    completed = False
+
+    while completed is False:
+
+        if mode == 'encrypt':
+            caesar_encrypt()
+            check = input('Encrypt another message?[y/n]')
+
+            if check == 'y':
+                pass
+
+            else:
+                print('Exiting...')
+                completed = True
+
+        elif mode == 'decrypt':
+            caesar_decrypt()
+            check = input('Decrypt another message?[y/n]')
+
+            if check == 'y':
+                pass
+
+            else:
+                print('Exiting...')
+                completed = True
+
+        else:
+            print('Invalid selection, only choose between [encrypt][decrypt]\nProgram stopping...')
+            break
+
+except KeyboardInterrupt:
+    print('Program terminated by user.\nExiting...')
+    
