4498d7228199403ab0db9b905a6ca4a3c8d0e138
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2019-12-31 11:12:28 -0800

    Create level_03.py

---
+++ pythonchallenge/level_03.py
@@ -0,0 +1,18 @@
+#http://www.pythonchallenge.com/pc/def/equality.html
+'''
+Based on web page, I think it'll need a re solution that looks for three capital letters that surround a
+lowecase letter on either side.
+
+Example:
+AEGyUIO = True
+YHEwNYH = True
+NEHBxHH = False
+JxehHEN = False
+'''
+import urllib.request
+import re
+from collections import Counter
+html = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/equality.html').read().decode()
+data = re.findall("<!--(.*?)-->", html, re.DOTALL)[-1]
+c = Counter(data)
+print(c.most_common())
