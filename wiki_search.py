2fc1e1711ceb10ddfe250acd4166a393a81ab194
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-03 10:40:24 -0800

    Update wiki_search.py

--- wiki_search.py
+++ wiki_search.py
@@ -5,12 +5,31 @@ try:
     program_running = True
 
     while program_running is True:
-        query = input('\nEnter something to lookup(Or press enter to exit):\n')
+        query = input('\nEnter something to lookup, type "?" for a random suggestion or press [enter] to exit:\n')
+
         if query == "":
             program_running = False
+
+        elif query == '?':
+            print(wikipedia.random(pages=1))
+
         else:
             search = wikipedia.page(query)
-            print(search.summary)
-        
+            mode = input('\n[summary] full[article] or [references]?\n')
+            mode = mode.strip()
+            mode = mode.lower()
+
+            if mode == 'summary':
+                print(search.summary)
+
+            elif mode == 'article':
+                print(search.content)
+
+            elif mode == 'references':
+                print(search.references)
+
+            else:
+                print('Invalid selection.')
+
 except KeyboardInterrupt:
     print('\nProgram terminated.\n')
