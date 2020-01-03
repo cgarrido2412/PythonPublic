297f688bf56d71040271e32b5f8206a814240719
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-03 10:41:13 -0800

    Update wiki_search.py

--- wiki_search.py
+++ wiki_search.py
@@ -5,7 +5,7 @@ try:
     program_running = True
 
     while program_running is True:
-        query = input('\nEnter something to lookup, type "?" for a random suggestion or press [enter] to exit:\n')
+        query = input('\nEnter something to lookup, type [?] for a random suggestion or press [enter] to exit:\n')
 
         if query == "":
             program_running = False
