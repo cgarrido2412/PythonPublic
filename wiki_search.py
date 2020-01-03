1b9dfb8b068aa799d70dd5514c25f924ff100e71
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-03 10:25:15 -0800

    Update wiki_search.py

--- wiki_search.py
+++ wiki_search.py
@@ -1,12 +1,16 @@
+#!Python3
 import wikipedia
 
 try:
     program_running = True
 
     while program_running is True:
-        query = input('\nEnter something to lookup:\n')
-        search = wikipedia.page(query)
-        print(search.summary)
+        query = input('\nEnter something to lookup(Or press enter to exit):\n')
+        if query == "":
+            program_running = False
+        else:
+            search = wikipedia.page(query)
+            print(search.summary)
         
 except KeyboardInterrupt:
     print('\nProgram terminated.\n')
