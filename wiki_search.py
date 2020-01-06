6043bbffa6f990402a794b95b73d69550991810a
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-06 13:27:40 -0800

    Update wiki_search.py

--- wiki_search.py
+++ wiki_search.py
@@ -7,7 +7,10 @@ def start():
         program_running = True
 
         while program_running is True:
-            query = input('\nEnter something to lookup, type [?] for a random article summary or press [enter] to exit:\n')
+            query = input('''
+Type [?] for a random article summary.
+Include [article] at the end of your search to print the full article.
+Enter something to lookup:\n''')
 
             if query == "":
                 program_running = False
@@ -17,11 +20,16 @@ def start():
                 result = wikipedia.page(result)
                 print(result.summary)
 
+            elif '[article]' in query:
+                search = wikipedia.page(query)
+                print(search.content)
+
             else:
                 search = wikipedia.page(query)
                 print(search.summary)
 
     except KeyboardInterrupt:
         print('\nProgram terminated.\n')
-
+        
 start()
+
