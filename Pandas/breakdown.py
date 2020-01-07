35e2e4e2fb1fe5eaa68b75c77dab1d211e277597
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-07 15:16:05 -0800

    Update breakdown.py

--- Pandas/breakdown.py
+++ Pandas/breakdown.py
@@ -3,7 +3,6 @@ storeOpen = 9
 storeClose = 22
 string1 = '2019-08-11 22:31:42'
 string2 = '2019-08-11 23:33:51'
-
 def breakdown(x,y):
     string1 = x.split() 
     variable2 = string1[1] 
@@ -11,7 +10,6 @@ def breakdown(x,y):
     hour = int(dateVariable2[0]) 
     minute = int(dateVariable2[1]) 
     seconds = int(dateVariable2[2])
-
     #For uptime
     string1B = y.split()
     variable2B = string1B[1]
@@ -19,9 +17,7 @@ def breakdown(x,y):
     hourB = int(dateVariable2B[0])
     minuteB = int(dateVariable2B[1])
     secondsB = int(dateVariable2B[2])
-
     if hour or hourB in range(storeOpen, storeClose):
-
         if hourB not in range(storeOpen, storeClose):
             if hourB > 22:
                 hourB = 22
@@ -29,7 +25,6 @@ def breakdown(x,y):
                 secondsB = 0
             else:
                 pass
-
         if hour not in range(storeOpen, storeClose):
             if hour < 9:
                 hour = 9
@@ -37,7 +32,6 @@ def breakdown(x,y):
                 seconds = 0
             else:
                 pass
-
         if hourB > hour:
             sumMinutes = (hourB - hour)*60
             sumMinutes = sumMinutes + (minuteB - minute)
@@ -46,14 +40,11 @@ def breakdown(x,y):
             else:
                 pass
             print(sumMinutes)
-
         elif hourB == hour:
             sumMinutes = minuteB - minute
             if sumMinutes < 0:
                 sumMinutes = 0
             print(sumMinutes)
-        
     else:
         print(0)
-
 print(breakdown(string1,string2))
