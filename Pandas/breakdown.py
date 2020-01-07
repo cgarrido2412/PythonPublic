ae30764fd1a80c19bdd8414a61aaa4a0d6ee3726
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-07 14:24:06 -0800

    Update breakdown.py

--- Pandas/breakdown.py
+++ Pandas/breakdown.py
@@ -1,38 +1,47 @@
-import pandas as pd
+#!Python 3
+storeOpen = 9
+storeClose = 22
+string1 = '2019-08-11 06:31:00'
+string2 = '2019-08-11 23:00:00'
 
-def breakdown(x, y):
-    #First breakdown downtime timestamp. Example string "2019-08-11 10:31:00"
-    string1 = x.split() #"2019-08-11" "10:31:00"
-    variable1 = string1[0] #"2019-08-11"
-    dateVariable = variable1.split('-') #"2019" "08" "11"
-    variable2 = string1[1] #"10:31:00"
-    dateVariable2 = variable2.split(':') #"10" "31" "00"
-    hour = int(dateVariable2[0]) #"10"
-    minute = int(dateVariable2[1]) #"31"
-    seconds = int(dateVariable2[2]) #"00"
+def breakdown(x,y):
+    string1 = x.split() 
+    variable2 = string1[1] 
+    dateVariable2 = variable2.split(':') 
+    hour = int(dateVariable2[0]) 
+    minute = int(dateVariable2[1]) 
+    seconds = int(dateVariable2[2])
 
     #For uptime
     string1B = y.split()
-    variable1B = string1B[0]
-    dateVariableB = variable1B.split('-')
     variable2B = string1B[1]
     dateVariable2B = variable2B.split(':')
     hourB = int(dateVariable2B[0])
     minuteB = int(dateVariable2B[1])
     secondsB = int(dateVariable2B[2])
 
-    if hourB > hour:
-        sumMinutes = (hourB - hour)*60
-        sumMinutes = sumMinutes + (minuteB - minute)
-        print(sumMinutes)
+    if hour or hourB in range(storeOpen, storeClose):
+
+        if hourB not in range(storeOpen, storeClose):
+            hourB = 22
+            minuteB = 0
+            secondsB = 0
+
+        if hour not in range(storeOpen, storeClose):
+            hour = 9
+            minute = 0
+            seconds = 0
+
+        if hourB > hour:
+            sumMinutes = (hourB - hour)*60
+            sumMinutes = sumMinutes + (minuteB - minute)
+            print(sumMinutes)
+
+        elif hourB == hour:
+            sumMinutes = (minuteB - minute)
+            print(sumMinutes)
         
-    elif hourB == hour:
-        sumMinutes = (minuteB - minute)
-        print(sumMinutes)
+    else:
+        print(0)
 
-data = pd.read_excel('E:\Savers\Python\Python3 - Master\lab.xlsx')
-
-for index, row in data.iterrows():
-    data1 = str(row['Adjusted_Down'])
-    data2 = str(row['Adjusted_Up'])
-    breakdown(data1, data2)
+print(breakdown(string1,string2))
