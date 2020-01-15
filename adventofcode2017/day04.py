917ffe6f75b9cd7e02805dbb8e89187f2da9c3f0
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-15 13:34:35 -0800

    Create day04.py

---
+++ adventofcode2017/day04.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+'''
+A new system policy has been put in place that requires all accounts to use a passphrase instead of simply a
+password. A passphrase consists of a series of words (lowercase letters) separated by spaces.
+
+To ensure security, a valid passphrase must contain no duplicate words.
+
+For example:
+
+aa bb cc dd ee is valid.
+aa bb cc dd aa is not valid - the word aa appears more than once.
+aa bb cc dd aaa is valid - aa and aaa count as different words.
+'''
+
+def check_duplicates(some_list):
+    if len(some_list) == len(set(some_list)):
+        return False
+    else:
+        return True
+
+def deduplicate(x):
+    return list(dict.fromkeys(x))
+
+puzzle = open('lab.txt', 'r').read()
+puzzle = puzzle.split('\n')
+
+new_list = []
+for i in range(len(puzzle)):
+    x = puzzle[i].split()
+    new_list.append(x)
