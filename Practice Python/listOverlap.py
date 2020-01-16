e973b530d9a51ad60059b6e65d5f841afce8219e
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-16 13:50:27 -0800

    Update listOverlap.py

--- Practice Python/listOverlap.py
+++ Practice Python/listOverlap.py
@@ -12,9 +12,12 @@ print('Common between A and B:', list(commonList))
 Another way:
 
 #! /usr/bin/env python3
+import random
+a = random.sample(range(1, 100), 10)
+b = random.sample(range(1, 100), 10)
+c = [x for x in a if x in b]
+print('List A:', a)
+print('List B:', b)
+print('Common elements:', c)
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-c = [x for x in b if x in a]
-print(c)
 '''
