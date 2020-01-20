f57f43dd8bb915708f7c9d61272fe968a3caac46
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-20 13:18:18 -0800

    Update MASTER.py

--- Practice Python/MASTER.py
+++ Practice Python/MASTER.py
@@ -46,12 +46,7 @@ def list_less_than_ten(): #XX
     and write a program that prints out all the elements of the list that are less than 5.
     '''
     a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-    b = []
-    for i in a:
-        if i < 5:
-            b.append(i)
-        else:
-            pass
+    b = [x for x in a if x < 10]
     print(b)
 
 def divisor_list(): #XX
