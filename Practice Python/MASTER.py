ff037afd3e70cc20c4eec60b9df289986895f4fb
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-21 08:11:16 -0800

    Update MASTER.py

--- Practice Python/MASTER.py
+++ Practice Python/MASTER.py
@@ -54,8 +54,7 @@ def divisor_list(): #XX
     Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
     '''
     number = int(input('Enter a number:\n'))
-    divisor_list = [x for x in range(1, number+1) if number % x == 0]
-    print(divisor_list)
+    print([x for x in range(1, number + 1) if number % x == 0])
 
 def list_overlap(): #XX
     '''
