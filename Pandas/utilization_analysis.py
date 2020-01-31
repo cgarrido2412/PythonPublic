464c5ea84152fb0b107063ef3926d7ff917cfac6
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-31 09:33:47 -0800

    Update utilization_analysis.py

--- Pandas/utilization_analysis.py
+++ Pandas/utilization_analysis.py
@@ -9,33 +9,26 @@ def file_test(file):
                 print('Unable to open:', file)
                 exit()
 
+def integer_format(y):
+        for x in range(len(data[y])):
+                data[y][x] = data[y][x].replace(' Mbps', '')
+
 file = r'Ben Test Utilization.xls'
 file_test(file)
 data = pd.read_excel(file, header=[2])
 final_data = pd.read_excel(file, header=[2])
 
-for x in range(len(data['Average Receive bps'])):
-        data['Average Receive bps'][x] = data['Average Receive bps'][x].replace(' Mbps', '')
-
-for x in range(len(data['Peak Receive bps'])):
-        data['Peak Receive bps'][x] = data['Peak Receive bps'][x].replace(' Mbps', '')
-
-for x in range(len(data['Received Bandwidth'])):
-        data['Received Bandwidth'][x] = data['Received Bandwidth'][x].replace(' Mbps', '')
-
-for x in range(len(data['Average Transmit bps'])):
-        data['Average Transmit bps'][x] = data['Average Transmit bps'][x].replace(' Mbps', '')
-
-for x in range(len(data['Peak Transmit bps'])):
-        data['Peak Transmit bps'][x] = data['Peak Transmit bps'][x].replace(' Mbps', '')
-
-for x in range(len(data['Transmit Bandwidth'])):
-        data['Transmit Bandwidth'][x] = data['Transmit Bandwidth'][x].replace(' Mbps', '')
+integer_format('Average Receive bps')
+integer_format('Peak Receive bps')
+integer_format('Received Bandwidth')
+integer_format('Average Transmit bps')
+integer_format('Peak Transmit bps')
+integer_format('Transmit Bandwidth')
 
 final_data['Average Receive Utilization %'] = (data['Average Receive bps'].astype(float)/data['Received Bandwidth'].astype(float))*100
 final_data['Peak Receive Utilization %'] = (data['Peak Receive bps'].astype(float)/data['Received Bandwidth'].astype(float))*100
 final_data['Average Upload Utilization %'] = (data['Average Transmit bps'].astype(float)/data['Transmit Bandwidth'].astype(float))*100
 final_data['Peak Upload Utilization %'] = (data['Peak Transmit bps'].astype(float)/data['Transmit Bandwidth'].astype(float))*100
 
-final_data.to_excel('lab.xls', 'w+')
-subprocess.call([r'C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE', r'C:\Users\cgarrido\Desktop\lab.xls'])
+final_data.to_excel('lab.xlsx', 'w+')
+subprocess.call([r'C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE', r'C:\Users\cgarrido\Desktop\lab.xlsx'])
