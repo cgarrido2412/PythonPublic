6161e450dc2a417ef8cb8d00c2f063e59d4ba7a7
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-02-05 09:36:10 -0800

    Update top_news.py

--- GeeksforGeeks/top_news.py
+++ GeeksforGeeks/top_news.py
@@ -1,31 +1,26 @@
-# importing requests package 
-import requests	 
+import requests
+'''
+Author: Charles Garrido
+
+Script uses an newsapi to grab the top 10 headlines from BBC
+Then prints the headlines into the terminal
+'''
 
 def NewsFromBBC(): 
-	
-	# BBC news api 
 	main_url = " https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=4dbc17e007ab436fb66416009dfb59a8"
-
-	# fetching data in json format 
 	open_bbc_page = requests.get(main_url).json() 
-
-	# getting all articles in a string article 
 	article = open_bbc_page["articles"] 
-
-	# empty list which will 
-	# contain all trending news 
 	results = [] 
-	
 	for ar in article: 
 		results.append(ar["title"]) 
-		
 	for i in range(len(results)): 
-		
-		# printing all trending news 
 		print(i + 1, results[i])				 
 
-# Driver Code 
-if __name__ == '__main__': 
-	
-	# function call 
-	NewsFromBBC() 
+if __name__ == '__main__':
+        program_running = True
+        while program_running is True:
+                NewsFromBBC()
+                kill_program = input('Press [enter] to quit.\n')
+                if kill_program == '':
+                        exit()
+                
