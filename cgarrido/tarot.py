e032e24e66ca6e33efd61dceab7acd4c9755f1e4
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-02-12 10:12:59 -0800

    Update tarot.py

--- cgarrido/tarot.py
+++ cgarrido/tarot.py
@@ -8,7 +8,7 @@ import time
 
 class Card(object):
     card_values = {
-        'Ace': 11,  # value of the ace is high until it needs to be low
+        'Ace': 11,  
         '2': 2,
         '3': 3,
         '4': 4,
@@ -23,40 +23,25 @@ class Card(object):
         'King': 10
     }
     def __init__(self, suit, rank):
-        """
-        :param suit: The face of the card, e.g. Spade or Diamond
-        :param rank: The value of the card, e.g 3 or King
-        """
         self.suit = suit.capitalize()
         self.rank = rank
         self.points = self.card_values[rank]
-
+        
 def ascii_version_of_card(*cards, return_string=True):
-    """
-    Instead of a boring text version of the card we render an ASCII image of the card.
-    :param cards: One or more card objects
-    :param return_string: By default we return the string version of the card, but the dealer hide the 1st card and we
-    keep it as a list so that the dealer can add a hidden card in front of the list
-    """
-    # we will use this to prints the appropriate icons for each card
     suits_name = ['Spades', 'Diamonds', 'Hearts', 'Clubs']
     suits_symbols = ['♠', '♦', '♥', '♣']
-    # create an empty list of list, each sublist is a line
     lines = [[] for i in range(9)]
     for index, card in enumerate(cards):
-        # "King" should be "K" and "10" should still be "10"
-        if card.rank == '10':  # ten is the only one who's rank is 2 char long
+        if card.rank == '10':  
             rank = card.rank
-            space = ''  # if we write "10" on the card that line will be 1 char to long
+            space = ''  
         else:
-            rank = card.rank[0]  # some have a rank of 'King' this changes that to a simple 'K' ("King" doesn't fit)
-            space = ' '  # no "10", we use a blank space to will the void
-        # get the cards suit in two steps
+            rank = card.rank[0]  
+            space = ' '  
         suit = suits_name.index(card.suit)
         suit = suits_symbols[suit]
-        # add the individual card on a line by line basis
         lines[0].append('┌─────────┐')
-        lines[1].append('│{}{}       │'.format(rank, space))  # use two {} one for char, one for space or char
+        lines[1].append('│{}{}       │'.format(rank, space))  
         lines[2].append('│         │')
         lines[3].append('│         │')
         lines[4].append('│    {}    │'.format(suit))
@@ -65,7 +50,6 @@ def ascii_version_of_card(*cards, return_string=True):
         lines[7].append('│       {}{}│'.format(space, rank))
         lines[8].append('└─────────┘')
     result = [''.join(line) for line in lines]
-    # hidden cards do not use string
     if return_string:
         return '\n'.join(result)
     else:
