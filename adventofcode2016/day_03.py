4723e3efb9c72268da4bf53706dc37bb0b7c732b
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-02-17 14:31:27 -0800

    Update day_03.py

--- adventofcode2016/day_03.py
+++ adventofcode2016/day_03.py
@@ -17,36 +17,48 @@ In your puzzle input, how many of the listed triangles are possible?
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
-puzzle = open(r'C:\Users\cgarrido\Desktop\lab.txt').read()
-lines = puzzle.split('\n')
+def file_test(x):
+    try:
+        open(x)
+    except FileNotFoundError:
+        print('Unable to open file.')
+        exit()
 
-rows = []
-for i in range(len(lines)):
-    number_line = lines[i].split(' ')
-    rows.append(number_line)
+def main():
+    file_test(r'C:\Users\cgarrido\Desktop\lab.txt')
+    puzzle = open(r'C:\Users\cgarrido\Desktop\lab.txt').read()
+    lines = puzzle.split('\n')
 
-string_list = []
-for i in range(len(rows)):
-    for x in rows[i]:
-        if x == '':
-            pass
+    rows = []
+    for i in range(len(lines)):
+        number_line = lines[i].split(' ')
+        rows.append(number_line)
+
+    string_list = []
+    for i in range(len(rows)):
+        for x in rows[i]:
+            if x == '':
+                pass
+            else:
+                string_list.append(x)
+
+    integer_list = [int(x) for x in string_list]
+
+    three_in_row_list = []
+    for group in chunker(integer_list, 3):
+        three_in_row_list.append(group)
+
+    triangle_count = 0
+    for x in range(len(three_in_row_list)):
+        results = []
+        results.append(three_in_row_list[x][0] + three_in_row_list[x][1] > three_in_row_list[x][2])
+        results.append(three_in_row_list[x][2] + three_in_row_list[x][1] > three_in_row_list[x][0])
+        results.append(three_in_row_list[x][2] + three_in_row_list[x][0] > three_in_row_list[x][1])
+        if sum(results) == 3:
+            triangle_count += 1
         else:
-            string_list.append(x)
+            pass
+    print(triangle_count)
 
-integer_list = [int(x) for x in string_list]
-
-three_in_row_list = []
-for group in chunker(integer_list, 3):
-    three_in_row_list.append(group)
-
-triangle_count = 0
-for x in range(len(three_in_row_list)):
-    results = []
-    results.append(three_in_row_list[x][0] + three_in_row_list[x][1] > three_in_row_list[x][2])
-    results.append(three_in_row_list[x][2] + three_in_row_list[x][1] > three_in_row_list[x][0])
-    results.append(three_in_row_list[x][2] + three_in_row_list[x][0] > three_in_row_list[x][1])
-    if sum(results) == 3:
-        triangle_count += 1
-    else:
-        pass
-print(triangle_count)
+if __name__ == '__main__':
+    main()
