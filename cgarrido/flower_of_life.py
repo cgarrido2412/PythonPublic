a8fb3fc9476de39108574c22e0812d79163dc869
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-02-24 13:17:10 -0800

    Update flower_of_life.py

--- cgarrido/flower_of_life.py
+++ cgarrido/flower_of_life.py
@@ -5,12 +5,12 @@ def draw_circle(t):
     t.circle(50)
 
 def main_column():
-    starting_y = -300
-    while starting_y < 200:
+    starting_y = -150
+    while starting_y < 100:
         t.up()
         t.sety(starting_y)
         draw_circle(t)
-        starting_y += 100
+        starting_y += 50
 
 if __name__ == '__main__':
     try:
