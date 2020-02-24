f4ffe0e29677fab7cc816ae85268521e1b585bd1
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-02-24 12:45:46 -0800

    Create flower_of_life.py

---
+++ cgarrido/flower_of_life.py
@@ -0,0 +1,22 @@
+import turtle
+
+def draw_circle(t):
+    t.down()
+    t.circle(50)
+
+t = turtle.Turtle()
+t.up()
+t.sety(-300)
+draw_circle(t)
+t.up()
+t.sety(-200)
+draw_circle(t)
+t.up()
+t.sety(-100)
+draw_circle(t)
+t.up()
+t.sety(0)
+draw_circle(t)
+t.up()
+t.sety(100)
+draw_circle(t)
