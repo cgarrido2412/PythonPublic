c01fdbd9da7c2044a4b8259bdd8d5ff4fd194929
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-02-24 12:51:39 -0800

    Update flower_of_life.py

--- cgarrido/flower_of_life.py
+++ cgarrido/flower_of_life.py
@@ -4,19 +4,13 @@ def draw_circle(t):
     t.down()
     t.circle(50)
 
+def main_column():
+    starting_y = -300
+    while starting_y < 200:
+        t.up()
+        t.sety(starting_y)
+        draw_circle(t)
+        starting_y += 100
+
 t = turtle.Turtle()
-t.up()
-t.sety(-300)
-draw_circle(t)
-t.up()
-t.sety(-200)
-draw_circle(t)
-t.up()
-t.sety(-100)
-draw_circle(t)
-t.up()
-t.sety(0)
-draw_circle(t)
-t.up()
-t.sety(100)
-draw_circle(t)
+main_column()
