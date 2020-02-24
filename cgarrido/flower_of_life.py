06965445ecd78a5244fb30dc07bbefb91fbfe2e9
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-02-24 13:26:01 -0800

    Update flower_of_life.py

--- cgarrido/flower_of_life.py
+++ cgarrido/flower_of_life.py
@@ -16,6 +16,40 @@ if __name__ == '__main__':
     try:
         t = turtle.Turtle()
         main_column()
+        t.up()
+        starting_y = -125
+        t.sety(starting_y)
+        t.setx(45)
+        draw_circle(t)
+        t.up()
+        starting_y += 50
+        t.sety(starting_y)
+        draw_circle(t)
+        t.up()
+        starting_y += 50
+        t.sety(starting_y)
+        draw_circle(t)
+        t.up()
+        starting_y += 50
+        t.sety(starting_y)
+        draw_circle(t)
+        t.up()
+        starting_y = -125
+        t.sety(starting_y)
+        t.setx(-45)
+        draw_circle(t)
+        t.up()
+        starting_y += 50
+        t.sety(starting_y)
+        draw_circle(t)
+        t.up()
+        starting_y += 50
+        t.sety(starting_y)
+        draw_circle(t)
+        t.up()
+        starting_y += 50
+        t.sety(starting_y)
+        draw_circle(t)
     except turtle.Terminator:
         print('Program terminated.')
     except KeyboardInterrupt:
