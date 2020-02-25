b52832f2faea3a0e55ada97339a0d28082b482d7
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-02-25 09:02:51 -0800

    Update chakras_flower_of_life.py

--- cgarrido/chakras_flower_of_life.py
+++ cgarrido/chakras_flower_of_life.py
@@ -53,14 +53,13 @@ def outer_most_column():
 
 if __name__ == '__main__':
     try:
+        turtle.speed(0)
         t = turtle.Turtle()
         t.hideturtle()
         turtle.hideturtle()
         turtle.bgcolor('black')
-
         c = 0
         x = 0
-
         colors = [
         #reddish colors
         (1.00, 0.00, 0.00),(1.00, 0.03, 0.00),(1.00, 0.05, 0.00),(1.00, 0.07, 0.00),(1.00, 0.10, 0.00),
@@ -87,7 +86,6 @@ if __name__ == '__main__':
         (0.25, 0.00, 1.00),(0.30, 0.00, 1.00),(0.35, 0.00, 1.00),(0.40, 0.00, 1.00),(0.45, 0.00, 1.00),
         (0.50, 0.00, 1.00),(0.55, 0.00, 1.00),(0.60, 0.00, 1.00),(0.65, 0.00, 1.00),(0.70, 0.00, 1.00),
         (0.75, 0.00, 1.00),(0.80, 0.00, 1.00),(0.85, 0.00, 1.00),(0.90, 0.00, 1.00),(0.95, 0.00, 1.00)]
-
         while x < 1000:
             idx = int(c)
             color = colors[idx]
@@ -96,34 +94,27 @@ if __name__ == '__main__':
             turtle.right(98)
             x += 1
             c += 0.1
-
         t.up()
         t.home()
-
         #Flower of life begins
         t.pencolor('white')
         main_column()
-
         #right side
         t.up()
         t.setx(45)
         outer_column()
-
         #left side
         t.up()
         t.setx(-45)
         outer_column()
-
         #outer left
         t.up()
         t.setx(-90)
         outer_most_column()
-
         #outer right
         t.up()
         t.setx(90)
         outer_most_column()
-
         #perimeter circle
         t.up()
         t.home()
