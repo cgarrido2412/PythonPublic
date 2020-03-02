50f845ee4df284a05185a5823895e75bc688cf02
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-03-02 09:58:32 -0800

    Update wiki_search.py

--- cgarrido/wiki_search.py
+++ cgarrido/wiki_search.py
@@ -1,35 +1,32 @@
-#!Python3
+# /usr/bin/env python3
+'''
+Author: Charles Garrido
+'''
 
-def start():
-    import wikipedia
+import wikipedia
 
+def main():
     try:
         program_running = True
-
-        while program_running is True:
+        while program_running:
             query = input('''
 Type [?] for a random article summary.
 Include [article] at the end of your search to print the full article.
 Enter something to lookup:\n''')
-
             if query == "":
                 program_running = False
-
             elif query == '?':
                 result = (wikipedia.random(pages=1))
                 result = wikipedia.page(result)
                 print(result.summary)
-
             elif '[article]' in query:
                 search = wikipedia.page(query)
                 print(search.content)
-
             else:
                 search = wikipedia.page(query)
                 print(search.summary)
-
     except KeyboardInterrupt:
         print('\nProgram terminated.\n')
         
-start()
-
+if __name__ == '__main__':        
+    main()
