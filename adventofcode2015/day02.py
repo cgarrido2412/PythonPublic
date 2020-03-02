29f9dddaaa87f9799bb2fa2e1cb7299ba26598d8
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-03-02 13:58:01 -0800

    Update day02.py

--- adventofcode2015/day02.py
+++ adventofcode2015/day02.py
@@ -15,6 +15,21 @@ slack, for a total of 58 square feet.
 A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot
 of slack, for a total of 43 square feet.
 All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
+
+The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the
+length they need to order, which they would again like to be exact.
+
+The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any
+one face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect
+bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
+
+For example:
+
+A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of
+ribbon for the bow, for a total of 34 feet.
+A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of
+ribbon for the bow, for a total of 14 feet.
+How many total feet of ribbon should they order?
 '''
 
 class Gift:
@@ -31,6 +46,18 @@ class Gift:
                 (2 * self.width * self.height) +
                 (2 * self.height * self.length) + minimum)
         return area
+    def smallest_side(self):
+        side_one = (self.length*2) + (self.width*2)
+        side_two = (self.length*2) + (self.height*2)
+        side_three = (self.width*2) + (self.height*2)
+        side_list = [side_one, side_two, side_three]
+        smallest_side = min(side_list)
+        return smallest_side
+    def ribbon(self):
+        perimeter = self.smallest_side()
+        volume = self.length * self.width * self.height
+        total_ribbon = perimeter + volume
+        return total_ribbon
 
 def file_test(file):
     try:
@@ -54,10 +81,15 @@ if __name__ == '__main__':
         integers.append(extracted_integers)
 
     list_of_square_feet = []
+    total_ribbon = []
     for x in range(len(integers)):
         present = Gift(integers[x][0], integers[x][1], integers[x][2])
         area = present.wrapper()
+        ribbon_amount = present.ribbon()
         list_of_square_feet.append(area)
+        total_ribbon.append(ribbon_amount)
 
     total_square_feet = sum(list_of_square_feet)
+    total_length_of_ribbon = sum(total_ribbon)
     print(total_square_feet)
+    print(total_length_of_ribbon)
