81f8aac15859ea5f86b104c2664f540c9c4adae1
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-07-06 10:00:06 -0700

    Update wiki_search.py

--- cgarrido/wiki_search.py
+++ cgarrido/wiki_search.py
@@ -1,12 +1,11 @@
-# /usr/bin/env python3
+#! /usr/bin/env python3
+
 '''
 Author: Charles Garrido
-Last Modified: 2 July 2020
+Last Modified: 6 July 2020
 '''
 
-import wikipedia
-
-def main():
+def wiki():
     while True:
         try:
             query = input('Search:\n')
@@ -15,8 +14,11 @@ def main():
                 print(search.summary)
             except wikipedia.exceptions.DisambiguationError as e:
                 print(e)
+            except wikipedia.exceptions.PageError as e:
+                print(e)
         except KeyboardInterrupt:
             print('Program terminated.')
-        
-if __name__ == '__main__':        
-    main()
+            exit()
+
+if __name__ == "__main__":
+    wiki()
