e88dc4366cd666a5dcac308358cf15c9127321da
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-09-04 09:32:57 -0700

    Create winsound.py

---
+++ GeeksforGeeks/winsound.py
@@ -0,0 +1,230 @@
+from winsound import Beep
+import time
+
+'''
+Super mario bros theme... sort of
+'''
+
+def theme_song():
+    Beep(659, 125)
+    Beep(659, 125)
+    time.sleep(0.125)
+    Beep(659, 125)
+    time.sleep(0.167)
+    Beep(523, 125)
+    Beep(659, 125)
+    time.sleep(0.125)
+    Beep(784, 125)
+    time.sleep(0.375)
+    Beep(392, 125)
+    time.sleep(0.375)
+    Beep(523, 125)
+    time.sleep(0.250)
+    Beep(392, 125)
+    time.sleep(0.250)
+    Beep(330, 125)
+    time.sleep(0.250)
+    Beep(440, 125)
+    time.sleep(0.125)
+    Beep(494, 125)
+    time.sleep(0.125)
+    Beep(466, 125)
+    time.sleep(0.42)
+    Beep(440, 125)
+    time.sleep(0.125)
+    Beep(392, 125)
+    time.sleep(0.125)
+    Beep(659, 125)
+    time.sleep(0.125)
+    Beep(784, 125)
+    time.sleep(0.125)
+    Beep(880, 125)
+    time.sleep(0.125)
+    Beep(698, 125)
+    Beep(784, 125)
+    time.sleep(0.125)
+    Beep(659, 125)
+    time.sleep(0.125)
+    Beep(523, 125)
+    time.sleep(0.125)
+    Beep(587, 125)
+    Beep(494, 125)
+    time.sleep(0.125)
+    Beep(523, 125)
+    time.sleep(0.250)
+    Beep(392, 125)
+    time.sleep(0.250)
+    Beep(330, 125)
+    time.sleep(0.250)
+    Beep(440, 125)
+    time.sleep(0.125)
+    Beep(494, 125)
+    time.sleep(0.125)
+    Beep(466, 125)
+    time.sleep(0.42)
+    Beep(440, 125)
+    time.sleep(0.125)
+    Beep(392, 125)
+    time.sleep(0.125)
+    Beep(659, 125)
+    time.sleep(0.125)
+    Beep(784, 125)
+    time.sleep(0.125)
+    Beep(880, 125)
+    time.sleep(0.125)
+    Beep(698, 125)
+    Beep(784, 125)
+    time.sleep(0.125)
+    Beep(659, 125)
+    time.sleep(0.125)
+    Beep(523, 125)
+    time.sleep(0.125)
+    Beep(587, 125)
+    Beep(494, 125)
+    time.sleep(0.375)
+    Beep(784, 125)
+    Beep(740, 125)
+    Beep(698, 125)
+    time.sleep(0.42)
+    Beep(622, 125)
+    time.sleep(0.125)
+    Beep(659, 125)
+    time.sleep(0.167)
+    Beep(415, 125)
+    Beep(440, 125)
+    Beep(523, 125)
+    time.sleep(0.125)
+    Beep(440, 125)
+    Beep(523, 125)
+    Beep(587, 125)
+    time.sleep(0.250)
+    Beep(784, 125)
+    Beep(740, 125)
+    Beep(698, 125)
+    time.sleep(0.42)
+    Beep(622, 125)
+    time.sleep(0.125)
+    Beep(659, 125)
+    time.sleep(0.167)
+    Beep(698, 125)
+    time.sleep(0.125)
+    Beep(698, 125)
+    Beep(698, 125)
+    time.sleep(0.625)
+    Beep(784, 125)
+    Beep(740, 125)
+    Beep(698, 125)
+    time.sleep(0.42)
+    Beep(622, 125)
+    time.sleep(0.125)
+    Beep(659, 125)
+    time.sleep(0.167)
+    Beep(415, 125)
+    Beep(440, 125)
+    Beep(523, 125)
+    time.sleep(0.125)
+    Beep(440, 125)
+    Beep(523, 125)
+    Beep(587, 125)
+    time.sleep(0.250)
+    Beep(622, 125)
+    time.sleep(0.250)
+    Beep(587, 125)
+    time.sleep(0.250)
+    Beep(523, 125)
+    time.sleep(0.1125)
+    Beep(784, 125)
+    Beep(740, 125)
+    Beep(698, 125)
+    time.sleep(0.42)
+    Beep(622, 125)
+    time.sleep(0.125)
+    Beep(659, 125)
+    time.sleep(0.167)
+    Beep(415, 125)
+    Beep(440, 125)
+    Beep(523, 125)
+    time.sleep(0.125)
+    Beep(440, 125)
+    Beep(523, 125)
+    Beep(587, 125)
+    time.sleep(0.250)
+    Beep(784, 125)
+    Beep(740, 125)
+    Beep(698, 125)
+    time.sleep(0.42)
+    Beep(622, 125)
+    time.sleep(0.125)
+    Beep(659, 125)
+    time.sleep(0.167)
+    Beep(698, 125)
+    time.sleep(0.125)
+    Beep(698, 125)
+    Beep(698, 125)
+    time.sleep(0.625)
+    Beep(784, 125)
+    Beep(740, 125)
+    Beep(698, 125)
+    time.sleep(0.42)
+    Beep(622, 125)
+    time.sleep(0.125)
+    Beep(659, 125)
+    time.sleep(0.167)
+    Beep(415, 125)
+    Beep(440, 125)
+    Beep(523, 125)
+    time.sleep(0.125)
+    Beep(440, 125)
+    Beep(523, 125)
+    Beep(587, 125)
+    time.sleep(0.250)
+    Beep(622, 125)
+    time.sleep(0.250)
+    Beep(587, 125)
+    time.sleep(0.250)
+    Beep(523, 125)
+    print("Song finished")
+
+if __name__ == "__main__":
+    #theme_song()
+
+    #Yankee Doodle
+    import winsound;
+
+    beatlength = 300;
+
+    winsound.Beep(262, beatlength) # C
+    winsound.Beep(262, beatlength) # C
+    winsound.Beep(294, beatlength) # D
+    winsound.Beep(330, beatlength) # E
+
+    winsound.Beep(262, beatlength) # C
+    winsound.Beep(330, beatlength) # E
+    winsound.Beep(294, 2*beatlength) # D (double length)
+
+    winsound.Beep(262, beatlength) # C
+    winsound.Beep(262, beatlength) # C
+    winsound.Beep(294, beatlength) # D
+    winsound.Beep(330, beatlength) # E
+
+    winsound.Beep(262, 2*beatlength) # C (double length)
+    winsound.Beep(247, 2*beatlength) # B (double length)
+
+    winsound.Beep(262, beatlength) # C
+    winsound.Beep(262, beatlength) # C
+    winsound.Beep(294, beatlength) # D
+    winsound.Beep(330, beatlength) # E
+
+    winsound.Beep(349, beatlength) # F
+    winsound.Beep(330, beatlength) # E
+    winsound.Beep(294, beatlength) # D
+    winsound.Beep(262, beatlength) # C
+
+    winsound.Beep(247, beatlength) # B
+    winsound.Beep(196, beatlength) # G
+    winsound.Beep(220, beatlength) # A
+    winsound.Beep(247, beatlength) # B
+
+    winsound.Beep(262, 2*beatlength) # C (double length)
+    winsound.Beep(262, 2*beatlength) # C (double length)
+
