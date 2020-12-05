745ee9fefadbca0cc0316ba49f4ea91fe34eb140
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2020-12-04 16:46:47 -0800

    Update day04.py

--- adventofcode2020/day04.py
+++ adventofcode2020/day04.py
@@ -45,6 +45,71 @@ The fourth passport is missing two fields, cid and byr. Missing cid is fine, but
 According to the above rules, your improved system would report 2 valid passports.
 
 Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?
+
+--- Part Two ---
+The line is moving more quickly now, but you overhear airport security talking about how passports with invalid data are getting through. Better add some data validation, quick!
+
+You can continue to ignore the cid field, but each other field has strict rules about what values are valid for automatic validation:
+
+byr (Birth Year) - four digits; at least 1920 and at most 2002.
+iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+hgt (Height) - a number followed by either cm or in:
+If cm, the number must be at least 150 and at most 193.
+If in, the number must be at least 59 and at most 76.
+hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+pid (Passport ID) - a nine-digit number, including leading zeroes.
+cid (Country ID) - ignored, missing or not.
+Your job is to count the passports where all required fields are both present and valid according to the above rules. Here are some example values:
+
+byr valid:   2002
+byr invalid: 2003
+
+hgt valid:   60in
+hgt valid:   190cm
+hgt invalid: 190in
+hgt invalid: 190
+
+hcl valid:   #123abc
+hcl invalid: #123abz
+hcl invalid: 123abc
+
+ecl valid:   brn
+ecl invalid: wat
+
+pid valid:   000000001
+pid invalid: 0123456789
+Here are some invalid passports:
+
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+Here are some valid passports:
+
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+Count the number of valid passports - those that have all required fields and valid values. Continue to treat cid as optional. In your batch file, how many passports are valid?
 '''
 
 import ast
@@ -58,6 +123,32 @@ def validate(passport):
             return False
     return True
 
+def pay_attention(passport):
+    if not (1920 <= int(passport["byr"]) <= 2002):
+        return False
+    if not (2010 <= int(passport["iyr"]) <= 2020):
+        return False
+    if not (2020 <= int(passport["eyr"]) <= 2030):
+        return False
+    if passport["hgt"][-2:] == "cm":
+        if not (150 <= int(passport["hgt"][:-2]) <= 193):
+            return False
+    elif passport["hgt"][-2:] == "in":
+        if not (59 <= int(passport["hgt"][:-2]) <= 76):
+            return False
+    else:
+        return False
+    if passport["hcl"][0] == '#':
+        if not int(passport["hcl"][1:], 16):
+            return False
+    else:
+        return False
+    if passport["ecl"] not in eye_colors:
+        return False
+    if len(passport["pid"]) != 9 or passport["pid"].isnumeric() == False:
+        return False
+    return True    
+
 if __name__ == "__main__":
     #Load the puzzle and break into individual passports based on whitespace line
     puzzle_input = open(os.getenv('HOMEDRIVE')+os.getenv('HOMEPATH')+'\Desktop\Prisma API\Documents\puzzle_input.txt').read()
@@ -65,8 +156,14 @@ if __name__ == "__main__":
     #The prerequsite fields for a valid passport
     fields = ("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 
+    #Valid eye colors
+    eye_colors = ("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+
     #Valid passports
-    count = 0
+    valid_count = 0
+
+    #Real passports on second look
+    real_id = 0
 
     #Separate by line breaks for each individual passport. 
     passports = puzzle_input.split('\n\n')
@@ -77,6 +174,10 @@ if __name__ == "__main__":
         passports[x] = {k:v for (k,v) in [s.split(':') for s in passports[x].split(' ')]}
 
     for x in range(len(passports)):
-        count += validate(ast.literal_eval(str(passports[x])))
+        valid_count += validate(ast.literal_eval(str(passports[x])))
+        if validate(ast.literal_eval(str(passports[x]))):
+            real_id += pay_attention(ast.literal_eval(str(passports[x])))
 
-    print(count)
+    answer = 'The number of valid passports: {}\nAfter paying attention, these are the real ones: {}'.format(valid_count, real_id)
+
+    print(answer)
