a4132f660831acf4e5790dc5c95f11a55ebe4dc5
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2021-03-24 16:02:40 -0700

    Update stir_fry.py

--- stir_fry.py
+++ stir_fry.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
+'''
+Description:
+    This script was made to generate random stir fry recipes. 
+    It keeps the ingredient selection to seven items both for simplicity
+    and to make things easier for one bag / one trip shopping. 
+    The elements of each dish include, protein, aromatics, vegetables, a finishing acid and a garnish. 
+    Along with heat, sweet and salt, these are some pretty tasty meals!
+'''
+
 import random 
+
+#Function to generate a random recipe based on ingredients
+def create_recipe(protein, aromatic, vegetables, finishers, garnish):
+    shopping_list = []
+    ingredients = [protein, aromatic, vegetables, finishers, garnish]
+    for x in range(len(ingredients)):
+        if ingredients[x] == vegetables:
+            random_selection = random.sample(vegetables, 3)
+            for item in random_selection:
+                shopping_list.append(item)
+        else:
+            random_selection = random.choice(ingredients[x])
+            shopping_list.append(random_selection)
+    return shopping_list
 
 if __name__ == "__main__":
     #starting ingredient lists
@@ -15,18 +38,9 @@ if __name__ == "__main__":
     garnish = ['cilantro', 'basil', 'green onion', 'sesame seeds', 'peanuts', 'hot sauce']
 
     #putting together something at random
-    shopping_list = []
-    ingredients = [protein, aromatic, vegetables, finishers, garnish]
-    for x in range(len(ingredients)):
-        if ingredients[x] == vegetables:
-            random_selection = random.sample(vegetables, 3)
-            for item in random_selection:
-                shopping_list.append(item)
-        else:
-            random_selection = random.choice(ingredients[x])
-            shopping_list.append(random_selection)
+    shopping_list = create_recipe(protein, aromatic, vegetables, finishers, garnish)
 
-    #shopping list
+    #Output of shopping list
     recipe = '''
 Your stir fry recipe list!
 Protein: {}
