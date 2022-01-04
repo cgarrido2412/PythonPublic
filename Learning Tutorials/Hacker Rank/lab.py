44e9f4cee8e20b6653ca82fd8db98db958c17616
Charles Garrido <charlesgarrido@MacBook-Pro.domain>
2022-01-04 10:22:21 -0800

    pushing changes

---
+++ Learning Tutorials/Hacker Rank/lab.py
@@ -0,0 +1,17 @@
+from collections import OrderedDict
+
+if __name__== "__main__":
+    #define empty ordered dictionary, which counts occurences
+    dict = OrderedDict()
+
+    for i in range(int(input())):
+        #If input not in the dictionary, then add it
+        #else increment the counter
+        key = input()
+        if not key in dict.keys():
+            dict.update({key : 1})
+            continue
+        dict[key] += 1
+
+    print(len(dict.keys()))
+    print(*dict.values())
