a780e4633da70d5e32d53b5f9d21c66bb74fad41
ramadhuta <54080405+cgarrido2412@users.noreply.github.com>
2022-12-06 09:10:51 -0800

    Create day06.py

---
+++ Challenges/Advent Of Code/adventofcode2022/day06.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+from signal import signal 
+
+def signal_lock(packet_size):
+    for (x, character) in enumerate(data):
+        if len(set(data[x:(x+packet_size)])) == packet_size:
+            return x + packet_size 
+
+if __name__ == "__main__":
+    
+    data = open(os.getenv('HOMEDRIVE')+os.getenv('HOMEPATH')+'\Desktop\Python\Coding Challenges\\adventofcode2022\day06_input.txt').read()
+
+    print('Part one:', signal_lock(4)) #Part one correct and comlete 
+    print('Part two:', signal_lock(14)) #Part two correct and complete
